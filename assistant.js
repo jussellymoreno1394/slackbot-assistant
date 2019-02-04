fae21d792a1b1bde5de4aee12ce1ad21af385e00
Jusselly Moreno <32177779+jussellymoreno1394@users.noreply.github.com>
2019-02-04 13:30:59 -0500

    Update assistant.js

--- assistant.js
+++ assistant.js
@@ -5,7 +5,7 @@ const client = require('twilio')(accountSid, authToken);
 
 client.autopilot.assistants
                 .create({
-                   friendlyName: 'Git Bot assistant',
+                   friendlyName: 'Git Bot assistant', // Change the name if you want
                    uniqueName: 'git-bot-assistant'
                  })
                 .then(assistant => console.log(assistant.sid))
